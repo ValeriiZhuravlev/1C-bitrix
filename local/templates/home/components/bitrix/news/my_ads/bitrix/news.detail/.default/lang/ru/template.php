49daa35224e67800d59ba48be7b35bee4e90cad4
talllorenc <talllorenc@gmail.com>
2023-09-25 05:57:11 +0300

    Подключить шаблон для раздела Мои объявления 1

---
+++ local/templates/home/components/bitrix/news/my_ads/bitrix/news.detail/.default/lang/ru/template.php
@@ -0,0 +1,13 @@
+<?
+$MESS["NEW_PROPERTIES"] = "Новые объявления";
+$MESS["AREA"] = "Площадь";
+$MESS["BEDS"] = "Спальней";
+$MESS["BATHS"] = "Санузлов";
+$MESS["GARAGE"] = "Гараж";
+$MESS["MORE_INFO"] = "Больше информации";
+$MESS["DATE"] = "Дата";
+$MESS["ADD MATERIAL"] = "Галерея";
+
+
+$MESS["CT_BNL_ELEMENT_DELETE_CONFIRM"] = "Будет удалена вся информация, связанная с этой записью. Продолжить?";
+?>
